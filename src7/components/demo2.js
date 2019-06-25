3bafa763d721d76b4fc1a62b78fccb7d7cde34c4
ltnsee <ltnsee8@163.com>
2019-06-25 16:41:03 +0800

    1

--- src7/components/demo2.js
+++ src7/components/demo2.js
@@ -4,7 +4,7 @@ export default function demo2(){
 
     // 规定一个规则，返回一个新的被观察的数据
     // 当反复的改变observable观测的值的时候computed就会重新计算
-    const boole = computed(() => {value.get >= 0})
+    const boole = computed(() => value >= 0)
 
     // autorun就是监听computed观测的对象，和observable是一样的
     autorun(() => {
